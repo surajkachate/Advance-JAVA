fe76d9a11cb783d496a00e09863f893ae08e540b
surajkachate <surajkachate@gmail.com>
2026-04-06 09:36:40 +0530

    update Main.java

--- JDBC_Connection/Main.java
+++ JDBC_Connection/Main.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/testdb";
+        String url = "jdbc:mysql://localhost:3306/test";
         String user = "root";
         String password = "";
 
